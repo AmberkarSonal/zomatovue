17f9e5308581775299fafef66635c30fa0cd0168
AmberkarSonal <sonal.amberkar@cognizant.com>
2022-09-15 08:50:32 +0530

    deployment

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV == 'production'? '/vueDeployment/': '/'
+  publicPath: process.env.NODE_ENV == 'production'? '/zomatovue/foodapp': '/'
 })
